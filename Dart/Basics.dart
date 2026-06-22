c5b1b40ff2f57f032b9d21fe5752bc324ef92f0e
Anandhakumar <anandhaanavamani@gmail.com>
2026-06-22 09:54:30 +0530

    typeCasting

---
+++ Dart/Basics.dart
@@ -0,0 +1,7 @@
+void main(){
+  int a = 50;
+  double x = 10.20;
+  print("The a value is: $a");
+  print(x);
+  print("Welcome to the world of dart");
+}
---
+++ Dart/oop.dart
@@ -0,0 +1,30 @@
+class Students{
+    int age = 20;
+    String name = "Arjun";
+}    
+
+void main(){
+    Students S = Students();
+    S.age = 22;
+    print("The student Age is ${S.age}");
+    print("The student Age is ${S.name}");
+}
+
+// Access Specifiers
+        // private
+        // protected
+        // public
+// Encapsulation
+
+// inheritence
+    // single -> one parent-->one child
+    // multilevel ->grandpa-> father->son->kids
+    // hyrachical -> one parent-> multiple children
+    // hybrid -> mixes of two inheritence
+    // multiple->two parent one child
+// polymorphism
+    // method overloading
+    // method overridding
+// Abstraction
+    
+// Composition
