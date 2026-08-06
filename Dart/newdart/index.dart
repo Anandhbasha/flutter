2fbcfb2dcf26d0b554b4fa218932f6d21a236757
Anandhakumar <anandhaanavamani@gmail.com>
2026-08-06 10:10:07 +0530

    dart

--- Dart/newdart/index.dart
+++ Dart/newdart/index.dart
@@ -1,5 +1,25 @@
-void main(){
-  // datatype varibaleName = value;
-  int a = 10;
-  print(a);
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+
+void main (){
+  Map Person = {
+    'name': 'John Doe',
+    'age': 30,
+    'email': 'jd@gmail.com'
+    };
+    // "{
+    // 'name': 'John Doe',
+    // 'age': 30,
+    // 'email': 'jd@gmail.com'
+    // };"
+    String data = jsonEncode(Person);
+    print(data);
+    // print(Person['name']);
+    Map user = jsonDecode(data);
+    print(user['name']);
+    // API
+    Future <void> getData() async{
+      var res = await http.get(Uri.parse("https://fakestoreapi.com/products"));
+      print(res.body);
+    }
 }
