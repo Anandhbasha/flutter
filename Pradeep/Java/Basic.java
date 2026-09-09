476a100278fef6fdee8ce2fc5f7828515c33dfa2
Anandhakumar <anandhaanavamani@gmail.com>
2026-09-09 09:43:41 +0530

    datatypes

---
+++ Pradeep/Java/Basic.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Basic{
+    public static void main(String[] args){
+        System.out.print("Hello Welcome");
+        System.out.print("\nHello Welcome to Java");
+        Scanner sc = new Scanner(System.in);
+        System.out.println(sc.nextInt());
+        // data type
+        // int
+        // byte
+        // small
+        //long
+        int num = 50;
+        // float
+        float price = 50.25f;
+        // double
+        double salary = 60000.85;
+        // char
+        char grade = 'A';
+        // String
+        String name = "Pradeep";
+        System.out.println(price);
+
+    }
+}
