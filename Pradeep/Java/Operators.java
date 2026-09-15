31bd0c02aa1da1440e0c29cecfdfb44c13d487bf
Anandhakumar <anandhaanavamani@gmail.com>
2026-09-15 12:15:15 +0530

    operators

---
+++ Pradeep/Java/Operators.java
@@ -0,0 +1,59 @@
+public class Operators {
+    public static void main(String[] args) {
+        // int a = 10;
+        // int b = 20;
+        // // arithmetic
+        // // +,-,*,/,%
+        // System.out.println(a+b); //30
+        // System.out.println(b-a); //10
+        // System.out.println(b*a); //200
+        // System.out.println(b/a); //2 
+        // System.out.println(b%a); //0
+        // // Comparison
+        // // >,<,>=,<=,!,==
+        // System.out.println(b>a);
+        // System.out.println(b<a);
+        // System.out.println(b<=20);
+        // System.out.println(b!=20);
+        // System.out.println(b==20);
+
+        // // Logical
+        // // and or not
+        // int age = 19;
+        // String nationality = "Indian";
+        // String State = "TN";
+        // System.out.println(age>=18 && nationality=="Indian" && State == "BI");
+        // // or
+        // System.out.println(age>=18 || nationality=="USA" || State == "BI");
+
+        // System.out.println(!(age>=18));
+        // // age>X
+        // // age>X
+
+        // // assigment
+        // // =,+=,-=,*=,/=,%=
+        // int x = 20;
+        // x*=5;
+        // // x=x+5;
+        // System.out.println(x);
+        // // increment
+        // int x = 5;
+        // // ++x;
+        // // x++;
+        // System.out.println(x++);
+        // System.out.println(x);
+        // System.out.println(++x);
+        // // x=x+1
+        // // decrement
+
+
+        //ternary
+        // // condition?value:value2 
+        // float weight =64.00f;
+        // System.out.println(weight>=65 ? "He is Eliglible":"Not Elgible");
+
+        // instanceof
+        String name = "Pradeep";
+        System.out.println(name instanceof String);
+    }
+}
