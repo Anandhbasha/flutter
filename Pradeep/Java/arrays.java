441f11257da74d8146ee81651527876679bbc609
Anandhakumar <anandhaanavamani@gmail.com>
2026-09-22 10:04:02 +0530

    loops

---
+++ Pradeep/Java/arrays.java
@@ -0,0 +1,17 @@
+public class arrays {
+    public static void main(String[] args){
+        int arr[] = {10,20,30,40,50};
+        // index->10->0,1,2,3,4
+        System.out.println(arr[1]);
+        // length of array ->5
+
+        // 2d array
+        int [][]nums = {
+            {10,20,30},
+            {40,50,60}
+        };
+
+        System.out.println(nums[0][0]);
+        System.out.println(nums[1][1]);
+    }
+}
---
+++ Pradeep/Java/loops.java
@@ -0,0 +1,10 @@
+public class loops {
+    public static void main(String[] args){
+        // while(condition){}
+        int a =1;
+        while(a<=10){ //2<=10
+            System.out.println(a); //1 2
+            a++; //2 3
+        }
+    }
+}
