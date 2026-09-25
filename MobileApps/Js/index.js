b90ccef58dba32e0325febcb1caa103d41704a04
Anandhakumar <anandhaanavamani@gmail.com>
2026-09-25 11:00:44 +0530

    condi

---
+++ MobileApps/Js/index.js
@@ -0,0 +1,86 @@
+// // var 
+// // console.log(a);
+// // var a = 10
+// // console.log(a);
+// // // re declaration
+// // var a = 60
+// // console.log(a);
+// // // re assign
+// // a=70
+// // console.log(a);
+
+
+// //let 
+// // console.log(a);
+// // let a = 10
+// //const
+// // console.log(a);
+
+// // const  a = 10
+// // Datatypes
+// // premitive
+//     // number
+//     // num = 100
+//     // console.log(num);
+    
+//     // boolean
+//     //string
+//     //undefined
+//     //null
+//     //Nan
+
+// // non premitive
+//     // array 
+//     //Object
+
+// // let x =15521545415415
+// // console.log(typeof(x));
+
+
+// // console.log("Hello"+3);
+
+
+// // let arr = [10,20,30,40,50]
+// // // let arr1 = [10,true,"names",undefined,null]
+// // console.log(arr[0]);
+// // console.log(arr[1]);
+// // console.log(arr.length);
+
+// // let arr1 = [
+// //     [10,20,30],[50,70,80]
+// // ]
+// // console.log(arr1[0][1]);
+// // console.log(arr1[1][1]);
+
+
+// //index
+// //length-> no of elements
+
+
+// const person = {
+//     userName:"jaya",
+//     age:25,
+//     family:{
+//         dad:"xyz",
+//         mom:"abc",
+//         siblings:{
+//             brother:["kavin","kalai"]
+//         }
+//     }
+// }
+// console.log(person.family.siblings.brother[1]);
+
+// if
+//if else
+//3 -> 
+let temp = 19
+
+if(temp<20){
+    console.log("Switch off the both ac and fan");    
+}
+if(temp>32){
+    console.log("Switch on the ac");   
+}
+else{
+    console.log("Switch on the fan");   
+}
